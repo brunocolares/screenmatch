f455e1e295bf9d6db81d309dccca632127401c22
bruno <brunocolaresdev@gmail.com>
2024-06-14 22:24:13 -0300

    Iniciando desserialização de título

--- src/br/com/colares/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/colares/screenmatch/principal/PrincipalComBusca.java
@@ -1,5 +1,8 @@
 package br.com.colares.screenmatch.principal;
 
+import br.com.colares.screenmatch.modelos.Titulo;
+import com.google.gson.Gson;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -20,7 +23,13 @@ public class PrincipalComBusca {
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println("HTTP Endpoint: "+request.uri());
-        System.out.println("HTTP Response: "+ response.body());
+
+        String json = response.body();
+        System.out.println(json);
+
+        Gson gson = new Gson();
+        Titulo meuTitulo = gson.fromJson(json, Titulo.class);
+        System.out.println(meuTitulo);
+
     }
 }
